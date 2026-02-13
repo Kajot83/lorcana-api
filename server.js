e0b29220cde93cc11d78c2cc70e5b9e1a78f6f92
Kajot83 <jkonkalec@gmail.com>
2026-02-13 12:58:54 +0100

    Set up Express server with routes and middleware

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Server configuration and setup
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cardsRoutes from "./routes/cards.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/cards", cardsRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Lorcana API działa");
+});
+
+app.listen(3000, () => {
+  console.log("Server start na porcie 3000");
+});
