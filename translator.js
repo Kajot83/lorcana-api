dcfcfc306115a03f796f75fd305d1fa68b30bdbb
Kajot83 <jkonkalec@gmail.com>
2026-02-13 13:08:01 +0100

    Add translation function in translator.js
    Implement translation function using Google Translate API.

--- translator.js
+++ translator.js
@@ -1,1 +1,15 @@
-// Translation module
+import axios from "axios";
+
+export default async function translate(text) {
+  if (!text) return "";
+
+  try {
+    const res = await axios.get(
+      `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=pl&dt=t&q=${encodeURIComponent(text)}`
+    );
+
+    return res.data[0].map(t => t[0]).join("");
+  } catch {
+    return text;
+  }
+}
