62cafde9abc363c248105d7c07f706bf874366e3
Kajot83 <jkonkalec@gmail.com>
2026-02-13 12:58:39 +0100

    Add MySQL connection pool configuration

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
-// Database configuration and connection
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const db = await mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
